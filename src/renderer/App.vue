741ea58fa61ac8e9040c182c532564bf0aca2953
刘宗源 <940101379@qq.com>
2018-09-25 09:22:56 +0800

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'blockcity-mp-ide'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
